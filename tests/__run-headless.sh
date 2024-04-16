02dd08dd0d22b73ea77acf4be8169bde553ce879
Scott Davis <scott@thirstyhead.com>
2024-04-16 14:54:33 -0600

    automation section

---
+++ tests/__run-headless.sh
@@ -0,0 +1,1 @@
+lighthouse https://developer.chrome.com --only-categories='accessibility' --output='html,json,csv' --chrome-flags='--headless'
---
+++ tests/__run-with-custom-config.sh
@@ -0,0 +1,1 @@
+lighthouse https://developer.chrome.com --config-path=./__custom-config.js
